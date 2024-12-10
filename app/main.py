be18e6170197a8a73e4ffc4ef59c7f5e6f662eed
Nikhil Sirohi <56220880+Nikhil-Sirohi@users.noreply.github.com>
2024-12-10 18:56:12 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -27,29 +27,4 @@ if __name__ == "__main__":
 
 
 
-'''
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.api import auth, documents, rag
-from app.core.config import settings
 
-app = FastAPI(title=settings.PROJECT_NAME)
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Update this with your frontend URL
-    allow_credentials=True, #it allows cookies
-    allow_methods=["*"], #it allows HTTP methods 
-    allow_headers=["*"], #it allows headers 
-)
-
-
-app.include_router(auth.router,prefix="/api/auth")
-app.include_router(documents.router)
-app.include_router(rag.router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    '''
